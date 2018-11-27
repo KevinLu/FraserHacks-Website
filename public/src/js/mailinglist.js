a1067e45dfbd796ad2010a876b3e736665cebd58
davidli3100 <david.li3100@gmail.com>
2018-11-26 20:00:36 -0500

    why tf doesn't mailchimp work

--- public/src/js/mailinglist.js
+++ public/src/js/mailinglist.js
@@ -1,7 +1,7 @@
 document.getElementById('mailinglist').addEventListener('submit', submitForm);
 
 function submitForm(e){
-
+  
   document.querySelector('.mailinglist-alert').style.marginTop = '-2em';
   document.querySelector('.mailinglist-alert').style.opacity = '1';
 
