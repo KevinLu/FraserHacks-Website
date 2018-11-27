2fed681c4659499f441bede7ef0bdd62397a05b9
davidli3100 <david.li3100@gmail.com>
2018-11-26 20:25:57 -0500

    messy but finally wokrs

--- public/src/js/mailinglist.js
+++ public/src/js/mailinglist.js
@@ -1,14 +1,15 @@
-document.getElementById('mailinglist').addEventListener('submit', submitForm);
+// document.getElementById('mailinglist').addEventListener('submit', submitForm);
 
-function submitForm(e){
-  
-  document.querySelector('.mailinglist-alert').style.marginTop = '-2em';
-  document.querySelector('.mailinglist-alert').style.opacity = '1';
+// function submitForm(e){
 
-  setTimeout(function(){
-    document.querySelector('.mailinglist-alert').style.marginTop = '-4em';
-    document.querySelector('.mailinglist-alert').style.opacity = '0';
-  },3000);
 
-  document.getElementById('mailinglist').reset();
-}
+//   document.querySelector('.mailinglist-alert').style.marginTop = '-2em';
+//   document.querySelector('.mailinglist-alert').style.opacity = '1';
+
+//   setTimeout(function(){
+//     document.querySelector('.mailinglist-alert').style.marginTop = '-4em';
+//     document.querySelector('.mailinglist-alert').style.opacity = '0';
+//   },3000);
+
+//   document.getElementById('mailinglist').reset();
+// }
