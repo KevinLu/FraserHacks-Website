41dce3db21045f0b6935aec97d89cf604153b447
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2019-04-07 13:26:57 -0400

    Change firebase js to components we need

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -3,3 +3,5 @@ $(document).ready(function(){
         $("#hero").css("opacity", 1 - $(window).scrollTop() / ($('#hero').height()*0.8));
     });
 });
+
+console.log('%c Interested in volunteering/mentoring at FraserHacks? E-mail us at contact@fraserhacks.ca ', 'background: #222; color: #bada55');
